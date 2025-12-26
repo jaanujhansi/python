c096c7cc9713ae40e20fcd15139a18e2f85f6f36
jaanujhansi <jjhansi156@gmail.com>
2025-12-26 15:13:39 +0530

    Add files via upload

---
+++ star4.py
@@ -0,0 +1,9 @@
+n=5
+for i in range(5):
+      for j in range(4-i):
+            print(" ",end=" ")
+            for k in range(i+1):
+                print("* ",end=" ")
+                print() 
+
+          
